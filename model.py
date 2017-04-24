1b9f85f7047e32d570d42ea03984fec60ba63453
Xiaosong Lu <luxiaos@gmail.com>
2017-04-24 00:53:38 -0400

    Added the script, the model and the video

---
+++ model.py
@@ -0,0 +1,60 @@
+import csv
+import cv2
+import numpy as np
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		lines.append(line)
+		
+images = []
+measurements = []
+for line in lines:
+	for i in range(3):
+		source_path = line[i]
+		filename = source_path.split('\\')[-1]
+		current_path = './data/IMG/' + filename
+		image = cv2.imread(current_path)
+		images.append(image)
+		if i == 0:
+			measurement = float(line[3])
+		elif i == 1:
+			measurement = float(line[3]) + 0.2
+		elif i == 2:
+			measurement = float(line[3]) - 0.2
+		measurements.append(measurement)
+
+augmented_images, augmented_measurements = [], []
+for image,measurement in zip(images, measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image,1))
+	augmented_measurements.append(measurement*-1.0)
+		
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse',optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
+
+model.save('model.h5')
+exit()
